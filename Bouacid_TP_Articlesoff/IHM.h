b76a8148b10b1616add84c904cd7d8381c0ce985
abouacid <ayman.bouacid@lyceeastier.com>
2023-10-16 15:01:49 +0200

    changement 2

--- Bouacid_TP_Articlesoff/IHM.h
+++ Bouacid_TP_Articlesoff/IHM.h
@@ -1,8 +1,26 @@
 #pragma once
+#include "Article.h"
+#include "Gestion.h"
+#include <vector>
 
 using namespace std;
 
 class IHM
 {
+
+	Gestion* gestion = NULL;
+
+public:
+
+	IHM();
+	~IHM();
+
+	void Start(Gestion* gestion);
+private:
+	int AfficheMenu();
+	int ChoixArticle();
+	void Modifier(Article*);
+	void Afficher(Article*);
+	void AfficherTout();
 
 };
