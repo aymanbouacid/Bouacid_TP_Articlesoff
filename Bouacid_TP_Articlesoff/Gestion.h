f854a020e86500be6e172fa27252648a03ccc570
abouacid <ayman.bouacid@lyceeastier.com>
2023-10-16 15:15:55 +0200

    cahngement 4

--- Bouacid_TP_Articlesoff/Gestion.h
+++ Bouacid_TP_Articlesoff/Gestion.h
@@ -1,8 +1,24 @@
 #pragma once
+#include "Article.h"
+#include <vector>
 
 using namespace std;
 
 class Gestion
 {
+
+	vector<Article*>* ListArticle;
+	void lireFichier();
+	void sauvegarderFichier();
+
+public:
+	Gestion();
+	~Gestion();
+
+	int getTaille();
+
+	Article* Ajouter(string nomArticle);
+	Article* LireAt(int pos);
+	bool Supprimer(int pos);
 
 };
